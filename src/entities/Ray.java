239ebeadc99ee092548a95cc65b33e1ceb9a1328
em232 <em232@linux43.macs.hw.ac.uk>
2015-01-20 11:06:28 +0000

    Fixing rays

--- src/entities/Ray.java
+++ src/entities/Ray.java
@@ -8,7 +8,8 @@ public class Ray {
 	public Vector3f Position;
 
 	public Ray(Vector3f rayPosition, Vector3f rayDirection) {
-		// TODO Auto-generated constructor stub
+		Direction = rayDirection;
+		Position = rayPosition;
 	}
 
 }
