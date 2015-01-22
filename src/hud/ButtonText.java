c0c7d684a5faa2f6605ce324362d851f3f6248de
em232 <em232@localhost>
2015-01-22 16:18:56 +0000

    More

---
+++ src/hud/ButtonText.java
@@ -0,0 +1,14 @@
+package hud;
+
+public class ButtonText {
+	String text;
+	float x,y;
+	
+	public ButtonText(String text, float x, float y){
+		this.text = text;
+		this.x = x;
+		this.y = y;
+	}
+	
+
+}
