86bb7f4c613032fb0119e11492bca4c2158becd4
Euan Mutch <euan.mutch@gmail.com>
2015-05-25 21:20:11 +0100

    dad

--- src/newRendering/DisplayManager.java
+++ src/newRendering/DisplayManager.java
@@ -10,8 +10,8 @@ import org.lwjgl.opengl.PixelFormat;
 
 public class DisplayManager {
 	
-	private static final int WIDTH = 640;
-	private static final int HEIGHT = 360;
+	private static final int WIDTH = 1024;
+	private static final int HEIGHT = 1024;
 	private static final int FPS_CAP = 240;
 	private static float GUI_SCALE = 1f;
 	private static float GUI_SCALE_FACTOR = 0.5f;
