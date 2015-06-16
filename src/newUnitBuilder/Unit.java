1ad358704d50a6600bd810fe104d75625a28f7f7
Euan Mutch <euan.mutch@gmail.com>
2015-06-16 17:41:11 +0100

    more

---
+++ src/newUnitBuilder/Unit.java
@@ -0,0 +1,56 @@
+package newUnitBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import com.bulletphysics.dynamics.constraintsolver.TypedConstraint;
+
+
+
+
+
+
+
+import newEntities.PhysicsEntity;
+import newMain.Globals;
+import newModels.PhysicsModel;
+import newModels.TexturedPhysicsModel;
+import newPhysics.PhysicsProcessor;
+import newTextures.ModelTexture;
+import newobjConverter.OBJFileLoader;
+
+public class Unit {
+
+	List<PhysicsEntity> entities;
+	List<TypedConstraint> joints;
+	
+	public void setup(PhysicsProcessor processor) {
+		entities = new ArrayList<PhysicsEntity>();
+		joints = new ArrayList<TypedConstraint>();
+		
+		createBaseCube(processor);
+	}
+
+	private void createBaseCube(PhysicsProcessor processor) {
+		
+		PhysicsModel pmodel = OBJFileLoader.loadOBJtoVAOWithGeneratedPhysics("box", Globals.getLoader());
+		TexturedPhysicsModel boxModel = new TexturedPhysicsModel(pmodel, new ModelTexture(Globals.getLoader().loadTexture("box")));
+		
+		PhysicsEntity base = new PhysicsEntity(boxModel, new Vector3f(100, 0, -50), 0, 0, 0, 1000, 10, processor);
+		entities.add(base);
+	}
+
+	public List<PhysicsEntity> getEntities() {
+		return entities;
+	}
+
+	public List<TypedConstraint> getJoints() {
+		return joints;
+	}
+	
+	
+	
+	
+}
