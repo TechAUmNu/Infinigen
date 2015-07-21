4db28ef730e3ca25bcda9db06d0fafc620c8d48a
DracoLunaris <fraserssg@gmail.com>
2015-07-21 20:47:02 +0100

    more work on unit builder
    made methods to alow rapid box and constraint creation

--- src/main/java/com/ionsystems/infinigen/unitBuilder/Unit.java
+++ src/main/java/com/ionsystems/infinigen/unitBuilder/Unit.java
@@ -30,29 +30,12 @@ public class Unit {
 	}
 
 	private void createBaseCube(PhysicsProcessor processor) {
-			
-		PhysicsModel pmodel = OBJFileLoader.loadOBJtoVAOWithGeneratedPhysics("box", Globals.getLoader());
-		TexturedPhysicsModel boxModel = new TexturedPhysicsModel(pmodel, new ModelTexture(Globals.getLoader().loadTexture("box")));
-		
-		PhysicsEntity base = new PhysicsEntity(boxModel, new Vector3f(10, 5, 10), 0, 0, 0, 1000, 1, processor);
-		PhysicsEntity jointTest = new PhysicsEntity(boxModel, new Vector3f(20, 5, 10), 0, 0, 0, 1000, 1, processor);
-		Transform frameInA, frameInB;
-		frameInA = new Transform();
-		frameInB = new Transform();
-		frameInA.setIdentity();
-		frameInB.setIdentity();
-		frameInA.origin.set(0f,10f,10f);
-		frameInB.origin.set(0f,10f,10f);
-		Generic6DofConstraint x = new Generic6DofConstraint(base.getBody(),jointTest.getBody(),frameInA,frameInB,true);
-		javax.vecmath.Vector3f z = new javax.vecmath.Vector3f(0f,0f,0f);
-		x.setAngularLowerLimit(z);
-		x.setAngularUpperLimit(z);
-		x.setLinearLowerLimit(z);
-		x.setLinearUpperLimit(z);
-		processor.addConstraint((TypedConstraint)x);
+		PhysicsEntity base = makeBox(processor,10,10,10,0);
+		PhysicsEntity jointTest = makeBox(processor,10,10,20,1);
+		Generic6DofConstraint binding = BindEntities(processor,base,jointTest,0.0f,2.3f,0.0f);
 		entities.add(base);
 		entities.add(jointTest);
-		joints.add(x);
+		joints.add(binding);
 		System.out.println("Body Hash: "+  base.getBody().hashCode());
 		
 		//Now we can set the camera to look at it.
@@ -65,6 +48,30 @@ public class Unit {
 
 	public List<TypedConstraint> getJoints() {
 		return joints;
+	}
+	
+	public PhysicsEntity makeBox(PhysicsProcessor processor, int x, int y , int z, int mass){
+		PhysicsModel pmodel = OBJFileLoader.loadOBJtoVAOWithGeneratedPhysics("box", Globals.getLoader());
+		TexturedPhysicsModel boxModel = new TexturedPhysicsModel(pmodel, new ModelTexture(Globals.getLoader().loadTexture("box")));
+		return new PhysicsEntity(boxModel, new Vector3f(x, y, z), 0, 0, 0, 1, mass, processor);
+	}
+	
+	public Generic6DofConstraint BindEntities(PhysicsProcessor processor, PhysicsEntity entity1, PhysicsEntity entity2, float x, float y , float z){
+		Transform frameInA, frameInB;
+		frameInA = new Transform();
+		frameInB = new Transform();
+		frameInA.setIdentity();
+		frameInB.setIdentity();
+		frameInA.origin.set(x,y,z);
+		frameInB.origin.set(0f,0f,0f);
+		Generic6DofConstraint binding = new Generic6DofConstraint(entity1.getBody(),entity2.getBody(),frameInA,frameInB,true);
+		javax.vecmath.Vector3f lock = new javax.vecmath.Vector3f(0f,0f,0f);
+		binding.setAngularLowerLimit(lock);
+		binding.setAngularUpperLimit(lock);
+		binding.setLinearLowerLimit(lock);
+		binding.setLinearUpperLimit(lock);
+		processor.addConstraint((TypedConstraint)binding);
+		return binding;
 	}
 
 	public void highlight(int hashCode) {
